32550b2152d31f88b3602a486ab2e02bef091fdf
SpudGunMan <kellykeeton@yahoo.com>
2022-08-01 10:14:00 -0700

    fix text on large name

--- DisplayManager.py
+++ DisplayManager.py
@@ -296,7 +296,7 @@ class DisplayManager:
 
 		self.setTextColor(self.black)
 		self.setTextSize(30)
-		self.drawCenteredText(self.eventsTextRow, "Largest EQ in:" + max_location + "  Last EQ @" + self.eventTimeStringLong)
+		self.drawCenteredText(self.eventsTextRow, "HiMag in:" + max_location + "  Last EQ @" + self.eventTimeStringLong)
 		self.setTextSize(40)
 		self.setTextColor(self.white)
 		return True
