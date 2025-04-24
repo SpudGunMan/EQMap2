b4026220c8bdaf6ef0b93df038b1b5964ae2b9c0
SpudGunMan <kellykeeton@yahoo.com>
2025-04-24 12:44:38 -0700

    Update eqmap.sh

--- eqmap.sh
+++ eqmap.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 # if rpi in uname -a, run the script
 if [[ $(uname -a) == *"rpi"* ]]; then
-    # check /dev/fb0 has permissions for all
-    if [ ! -w /dev/fb0 ]; then
-        echo "Setting permissions for /dev/fb0"
-        sudo chmod 666 /dev/fb0
-    fi
+
     # replace user pi with whoami in the EQMap.desktop file
     if [ -f /home/pi/Desktop/EQMap.desktop ]; then
         sed -i "s|/home/pi|/home/$(whoami)|g" /home/pi/Desktop/EQMap.desktop
     fi
-    
+
     # copy the EQMap.desktop file to ~/.config/autostart
     if [ ! -f ~/.config/autostart/EQMap.desktop ]; then
         mkdir -p ~/.config/autostart
