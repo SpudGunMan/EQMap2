3d8b3e37622e17717542d256840bb322f880192a
SpudGunMan <kellykeeton@yahoo.com>
2025-04-24 12:41:25 -0700

    Update eqmap.sh

--- eqmap.sh
+++ eqmap.sh
@@ -6,6 +6,16 @@ if [[ $(uname -a) == *"rpi"* ]]; then
         echo "Setting permissions for /dev/fb0"
         sudo chmod 666 /dev/fb0
     fi
+    # replace user pi with whoami in the EQMap.desktop file
+    if [ -f /home/pi/Desktop/EQMap.desktop ]; then
+        sed -i "s|/home/pi|/home/$(whoami)|g" /home/pi/Desktop/EQMap.desktop
+    fi
+    
+    # copy the EQMap.desktop file to ~/.config/autostart
+    if [ ! -f ~/.config/autostart/EQMap.desktop ]; then
+        mkdir -p ~/.config/autostart
+        cp /home/pi/Desktop/EQMap.desktop ~/.config/autostart/
+    fi
 fi
 
 # Run the EQMap.py script
