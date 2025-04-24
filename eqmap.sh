457eddca401b1185b43c0a4170acb4b6aee27d57
SpudGunMan <kellykeeton@yahoo.com>
2025-04-24 12:13:17 -0700

    Update eqmap.sh

--- eqmap.sh
+++ eqmap.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# if rpi in uname -a, run the script
+if [[ $(uname -a) == *"rpi"* ]]; then
+    # check /dev/fb0 has permissions for all
+    if [ ! -w /dev/fb0 ]; then
+        echo "Setting permissions for /dev/fb0"
+        sudo chmod 666 /dev/fb0
+    fi
+fi
+
 # Run the EQMap.py script
 cd "$(dirname "$0")"
 python3 EQMap.py
