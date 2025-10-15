c89b44acbc8ecd396dba093e021420188b81fea5
SpudGunMan <kellykeeton@yahoo.com>
2025-10-15 11:24:57 -0700

    Update DisplayManager.py

--- DisplayManager.py
+++ DisplayManager.py
@@ -389,7 +389,7 @@ class DisplayManager:
 				# larger screens
 				self.drawText(x0 - 150, y0 + graph_height - 110, f"Freq Trend (hourly, last 24h {24 - start_idx}h left)")
 				self.drawText(x0 - 150, y0 + graph_height - 130, f"Start: {start_idx:02d}:00")
-				self.drawRightJustifiedText(x0 - 150, y0 + graph_height - 130, f"Max Events hour:{max_val}")
+				self.drawRightJustifiedText(y0 + graph_height - 130, f"Max Events hour:{max_val}")
 				# end larger screens
 			else:
 				# smaller screens
