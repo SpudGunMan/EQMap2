806be2f5bcb5a62494da40cb9a433b9a1fef3cf5
SpudGunMan <kellykeeton@yahoo.com>
2025-10-14 19:54:13 -0700

    Update DisplayManager.py

--- DisplayManager.py
+++ DisplayManager.py
@@ -408,7 +408,6 @@ class DisplayManager:
 						self.drawCenteredText((self.topTextRow + 160), "Active Region: " + activeregion_str)
 						self.drawCenteredText((self.topTextRow + 300), str(self.eventCount) + " events, last quake @" + self.eventTimeStringLong)
 						self.drawCenteredText((self.topTextRow + 430), "Yesterdays event count " + dayTrend_str + freq_trend)
-					time.sleep(20)
 					self.firstRun = False
 				except Exception as e:
 					print(f"Error displaying wash page: {e}")
