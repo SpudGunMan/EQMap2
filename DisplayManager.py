955cf2bfc119ab8b9f50ab7861b2b62511ae51fb
Kelly <kellykeeton@yahoo.com>
2026-03-04 22:13:17 -0800

    Update DisplayManager.py

--- DisplayManager.py
+++ DisplayManager.py
@@ -410,13 +410,11 @@ class DisplayManager:
 
 			if self.screenWidth > 1000:
 				self.setTextSize(20)
-				label_x = x0 - 10
-				label_y_offset = 190
-				self.drawText(label_x, y0 + graph_height - 130 + label_y_offset,
+				self.drawText(x0, y0 + graph_height - 130,
 					f"Events (last hour): {lastHoursEvents if lastHoursEvents is not None else 0}")
-				self.drawText(label_x, y0 + graph_height - 110 + label_y_offset,
+				self.drawText(x0, y0 + graph_height - 110,
 					f"Events (this hour): {thisHoursEvents if thisHoursEvents is not None else 0}")
-				self.drawRightJustifiedText(y0 + graph_height - 130 + label_y_offset,
+				self.drawRightJustifiedText(y0 + graph_height - 130,
 					f"Max Events/hour: {int(round(max_val))}")
 			else:
 				self.setTextSize(18)
